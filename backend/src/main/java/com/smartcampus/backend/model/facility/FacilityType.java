d4d11a759010abdde3b3c82299cf981ac8f9895a
Thiruni Wijerathne <thiruwijerathne@gmail.com>
2026-03-08 13:01:22 +0530

    Backend modified by adding folders and files

---
+++ backend/src/main/java/com/smartcampus/backend/model/facility/FacilityType.java
@@ -0,0 +1,5 @@
+package com.smartcampus.backend.model.facility;
+
+public class FacilityType {
+    
+}
---
+++ backend/src/main/java/com/smartcampus/backend/service/auth/UserService.java
@@ -0,0 +1,5 @@
+package com.smartcampus.backend.service.auth;
+
+public class UserService {
+    
+}
